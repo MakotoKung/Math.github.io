6619ec137be5fea63197e00cb9f3a167e7a34465
MakotoKung <angleoftail@gmail.com>
2022-09-06 22:32:05 +0700

    01

---
+++ java.js
@@ -0,0 +1,14 @@
+var start = document.getElementById('start');
+var op0 = document.getElementById('op0');
+var rec = document.getElementById('rec');
+
+window.onload = function(){
+    setTimeout(function(){
+        start.classList.remove('BG');
+        start.classList.add('BG-2');
+        op0.classList.remove('main');
+    },2000)
+    setTimeout(function(){
+        rec.classList.remove('main');
+    },8200)
+}
